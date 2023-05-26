f9d36b312d611be4bc3b8c837cdaa65f9b49be12
yassminch <yassminechayeb7@gmail.com>
2023-05-26 17:16:10 +0200

    committ

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Bloc;
 use App\Entity\Salle;
 use App\Form\SalleType;
 use App\Repository\SalleRepository;
@@ -9,70 +10,82 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 #[Route('/salle')]
 class SalleController extends AbstractController
 {
-    #[Route('/', name: 'app_salle_index', methods: ['GET'])]
-    public function index(SalleRepository $salleRepository): Response
-    {
-        return $this->render('salle/index.html.twig', [
-            'salles' => $salleRepository->findAll(),
-        ]);
-    }
+#[Route('/', name: 'app_salle_index', methods: ['GET'])]
+public function index(SalleRepository $salleRepository): Response
+{
+return $this->render('salle/index.html.twig', [
+'salles' => $salleRepository->findAll(),
+]);
+}
 
-    #[Route('/new', name: 'app_salle_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, SalleRepository $salleRepository): Response
-    {
-        $salle = new Salle();
-        $form = $this->createForm(SalleType::class, $salle);
-        $form->handleRequest($request);
+#[Route('/new', name: 'app_salle_new', methods: ['GET', 'POST'])]
+public function new(Request $request, SalleRepository $salleRepository): Response
+{
+$salle = new Salle();
+$form = $this->createForm(SalleType::class, $salle);
+$form->handleRequest($request);
+if ($form->isSubmitted() && $form->isValid()) {
+$criteria = array(
+'nomSalle' => $salle->getNomSalle(),
+'bloc' => $salle->getBloc()
+);
+$entite = $salleRepository->findOneBy($criteria);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $salleRepository->save($salle, true);
+if($entite != null){
+$salleRepository->save($salle, true);
+return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
+}
+else{
+throw new \Exception('Salle existe deja !');
+}
+}
 
-            return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
-        }
+return $this->renderForm('salle/new.html.twig', [
+'salle' => $salle,
+'form' => $form,
+]);
+}
 
-        return $this->renderForm('salle/new.html.twig', [
-            'salle' => $salle,
-            'form' => $form,
-        ]);
-    }
+#[Route('/{id}', name: 'app_salle_show', methods: ['GET'])]
+public function show(Salle $salle): Response
+{
+return $this->render('salle/show.html.twig', [
+'salle' => $salle,
+]);
+}
 
-    #[Route('/{id}', name: 'app_salle_show', methods: ['GET'])]
-    public function show(Salle $salle): Response
-    {
-        return $this->render('salle/show.html.twig', [
-            'salle' => $salle,
-        ]);
-    }
+#[Route('/{id}/edit', name: 'app_salle_edit', methods: ['GET', 'POST'])]
+public function edit(Request $request, Salle $salle, SalleRepository $salleRepository): Response
+{
+$form = $this->createForm(SalleType::class, $salle);
+$form->handleRequest($request);
 
-    #[Route('/{id}/edit', name: 'app_salle_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Salle $salle, SalleRepository $salleRepository): Response
-    {
-        $form = $this->createForm(SalleType::class, $salle);
-        $form->handleRequest($request);
+if ($form->isSubmitted() && $form->isValid()) {
+$salleRepository->save($salle, true);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $salleRepository->save($salle, true);
+return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
+}
 
-            return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
-        }
+return $this->renderForm('salle/edit.html.twig', [
+'salle' => $salle,
+'form' => $form,
+]);
+}
 
-        return $this->renderForm('salle/edit.html.twig', [
-            'salle' => $salle,
-            'form' => $form,
-        ]);
-    }
+#[Route('/{id}', name: 'app_salle_delete', methods: ['POST'])]
+public function delete(Request $request, Salle $salle, SalleRepository $salleRepository): Response
+{
+if ($this->isCsrfTokenValid('delete'.$salle->getId(), $request->request->get('_token'))) {
+$salleRepository->remove($salle, true);
+}
 
-    #[Route('/{id}', name: 'app_salle_delete', methods: ['POST'])]
-    public function delete(Request $request, Salle $salle, SalleRepository $salleRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$salle->getId(), $request->request->get('_token'))) {
-            $salleRepository->remove($salle, true);
-        }
-
-        return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
-    }
+return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
+}
 }
