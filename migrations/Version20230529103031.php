f83f71a5653dcfe38804baf77933fa0c1b517148
dadibabaahme <dadibabaahmed02@gmail.com>
2023-05-29 11:47:05 +0100

    final final version hopefully

--- migrations/Version20230529103031.php
+++ migrations/Version20230529103031.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230529100949 extends AbstractMigration
+final class Version20230529103031 extends AbstractMigration
 {
     public function getDescription(): string
     {
